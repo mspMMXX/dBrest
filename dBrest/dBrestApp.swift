60382038898ff164520b5f1c91f50719f9444130
Markus Platter <platter.markus@gmail.com>
2025-03-25 12:57:48 +0100

    Initial Commit

---
+++ dBrest/dBrestApp.swift
@@ -0,0 +1,17 @@
+//
+//  dBrestApp.swift
+//  dBrest
+//
+//  Created by Markus Platter on 25.03.25.
+//
+
+import SwiftUI
+
+@main
+struct dBrestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dBrestTests/dBrestTests.swift
@@ -0,0 +1,17 @@
+//
+//  dBrestTests.swift
+//  dBrestTests
+//
+//  Created by Markus Platter on 25.03.25.
+//
+
+import Testing
+@testable import dBrest
+
+struct dBrestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
