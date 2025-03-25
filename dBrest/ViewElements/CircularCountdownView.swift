1c787125c2ac28e2a461eb7d6b569caf47a75c5b
Markus Platter <platter.markus@gmail.com>
2025-03-25 16:39:27 +0100

    Circle-Progress-Bar -> Done

---
+++ dBrest/ViewElements/CircularCountdownView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularCountdownView.swift
+//  dBrest
+//
+//  Created by Markus Platter on 25.03.25.
+//
+
+import SwiftUI
+
+struct CircularCountdownView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularCountdownView()
+}
